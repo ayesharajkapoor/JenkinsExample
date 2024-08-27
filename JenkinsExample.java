36a2fc29bc0fffbee4a350e488f767afc8bae27e
ayesharajkapoor <ayesharajkapoor@gmail.com>
2024-08-27 19:28:07 +0530

    JenkinsExample file added

---
+++ JenkinsExample.java
@@ -0,0 +1,11 @@
+package selenium;
+
+public class JenkinsExample {
+
+	public static void main(String[] args) {
+		System.out.println(" Welcome to the Jenkins");
+		System.out.println("Ayesha");
+
+	}
+
+}
