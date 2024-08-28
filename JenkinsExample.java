fd7e594919ba49c64892de87aa234c04fbe56237
ayesharajkapoor <ayesharajkapoor@gmail.com>
2024-08-28 07:19:27 +0530

    Update JenkinsExample.java
    changed name

--- JenkinsExample.java
+++ JenkinsExample.java
@@ -6,7 +6,7 @@ public class JenkinsExample {
 		System.out.println(" Welcome to the Jenkins");
 		System.out.println("Ayesha Rajkapoor");
 		System.out.println("Rafeek Haroon");
-			System.out.println("Samhaan");
+			System.out.println("Muhammed Samhaan");
 	
 
 	}
