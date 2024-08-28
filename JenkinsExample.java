644f135bc8bcb5ed56d96c6f9469012f9d709030
ayesharajkapoor <ayesharajkapoor@gmail.com>
2024-08-28 07:46:09 +0530

    Update JenkinsExample.java
    chaned

--- JenkinsExample.java
+++ JenkinsExample.java
@@ -7,7 +7,7 @@ public class JenkinsExample {
 		System.out.println("Ayesha Rajkapoor");
 		System.out.println("Rafeek Haroon");
 			System.out.println("Muhammed Samhaan");
-		System.out.println("Rahmaan");
+		System.out.println("Abdur rahman");
 	
 
 	}
