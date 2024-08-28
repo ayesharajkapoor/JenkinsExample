90fb135ebcdd0ad0d27fa2d009091f7a369096fb
ayesharajkapoor <ayesharajkapoor@gmail.com>
2024-08-28 07:50:54 +0530

    Update JenkinsExample.java

--- JenkinsExample.java
+++ JenkinsExample.java
@@ -4,7 +4,7 @@ public class JenkinsExample {
 
 	public static void main(String[] args) {
 		System.out.println(" Welcome to the Jenkins");
-		System.out.println("Ayesha Rajkapoor");
+		System.out.println("Ayesha ");
 		System.out.println("Rafeek Haroon");
 			System.out.println("Muhammed Samhaan");
 		System.out.println("Abdur rahman");
