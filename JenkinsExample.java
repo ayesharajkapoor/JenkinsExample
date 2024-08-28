22860f81b89843c22cf80035b64bb273c9ac1e04
ayesharajkapoor <ayesharajkapoor@gmail.com>
2024-08-28 06:31:25 +0530

    Update JenkinsExample.java
    Added Rafeeq

--- JenkinsExample.java
+++ JenkinsExample.java
@@ -5,6 +5,7 @@ public class JenkinsExample {
 	public static void main(String[] args) {
 		System.out.println(" Welcome to the Jenkins");
 		System.out.println("Ayesha");
+		System.out.println("Rafeeq");
 
 	}
 
